9b5b1ca0e54472c43cfdb77d3264b8f8d32ca542
juapormon <juapormon@alum.us.es>
2021-02-12 11:56:06 +0100

    dockerconfig arreglado

--- docker/docker-settings.py
+++ docker/docker-settings.py
@@ -5,6 +5,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
+        'PASSWORD':'postgres',
         'HOST': 'db',
         'PORT': 5432,
     }
@@ -27,7 +28,7 @@ MODULES = [
     'voting',
 ]
 
-BASEURL = 'http://10.5.0.1:8001'
+BASEURL = 'http://10.5.0.1:8000'
 
 APIS = {
     'authentication': 'http://10.5.0.1:8001',
